c5bc04464846de31146e7f084f4edfd3c9213494
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-06 16:36:59 +0700

    Update map.js

--- map.js
+++ map.js
@@ -12,8 +12,7 @@ function onLocationFound(e) {
     L.marker(e.latlng).addTo(map)
     .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
     L.circle(e.latlng, radius).addTo(map);
-    if (radius < 10){
-        alert('stop')
+    if (radius < 5){
         map.stopLocate()
     }
 }
