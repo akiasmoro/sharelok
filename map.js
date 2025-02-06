40b159879d412c9fd15429697516da04a1f9fd54
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-06 12:57:20 +0700

    Update map.js

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ map.locate({setView: true, maxZoom: 16});
 function onLocationFound(e) {
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
-    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='GeeksForGeeks()'>Copy</button><div style='display:none' id='asu'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +"").openPopup();
+    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +"</div>").openPopup();
     L.circle(e.latlng, radius).addTo(map);
 }
 
@@ -22,15 +22,20 @@ function onLocationError(e) {
 
 map.on('locationerror', onLocationError);
 
-function GeeksForGeeks() {
+function Copy() {
+        var temp = $("<input>");
+        $("body").append(temp);
+    
+        var previewHeader = $("#copydata").text();
+        var contentTogether = previewHeader;
+    
+        temp.val(contentTogether).select();
+        document.execCommand("copy");
+        $("#thecopiedtext").text(contentTogether);
+        temp.remove();
+
     Swal.fire({
         title: "Sukse copy sharelok",
         icon: "success",
       });
-    var range = document.createRange();
-    range.selectNode(document.getElementById("asu"));
-    window.getSelection().removeAllRanges(); // clear current selection
-    window.getSelection().addRange(range); // to select text
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();// to deselect
 }
