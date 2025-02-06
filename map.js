fd1e1c7290bc2a657912fc42a9b0a3cd3855aeec
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-06 12:47:54 +0700

    Update map.js

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ map.locate({setView: true, maxZoom: 16});
 function onLocationFound(e) {
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
-    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, <br/> Klik <a href='https://www.google.com/maps/place/"+ e.latlng.lat +","+ e.latlng.lng +"'>disini</a> untuk copy link sharelok").openPopup();
+    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='GeeksForGeeks()'>Copy</button><div style='display:none' id='asu'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +"").openPopup();
     L.circle(e.latlng, radius).addTo(map);
 }
 
@@ -21,3 +21,16 @@ function onLocationError(e) {
 }
 
 map.on('locationerror', onLocationError);
+
+function GeeksForGeeks() {
+    Swal.fire({
+        title: "Sukse copy sharelok",
+        icon: "success",
+      });
+    var range = document.createRange();
+    range.selectNode(document.getElementById("asu"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+}
