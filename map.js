c4b66a35e61b68a42147fe806dc6c762a69e7470
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-06 14:16:44 +0700

    Update map.js

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {
     attribution: '© Google Satellite Map'
 }).addTo(map);
 
-map.locate({setView: true, maxZoom: 19});
+map.locate({setView: true, maxZoom: 18});
 
 function onLocationFound(e) {
     var radius = e.accuracy.toFixed(2);
