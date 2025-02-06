44e7d4a5de3405e3926f9a5015639f09151ff393
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-06 11:50:57 +0700

    Update map.js

--- map.js
+++ map.js
@@ -8,7 +8,7 @@ L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {
 map.locate({setView: true, maxZoom: 16});
 
 function onLocationFound(e) {
-    var radius = e.accuracy;
+    var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
     .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, <br/> Klik <a href='https://www.google.com/maps/place/"+ e.latlng.lat +","+ e.latlng.lng +"'>disini</a> untuk copy link sharelok").openPopup();
     L.circle(e.latlng, radius).addTo(map);
