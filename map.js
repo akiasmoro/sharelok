6ec4797a357a758920b0309aea2382976a4dcd94
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 09:16:18 +0700

    Update map.js

--- map.js
+++ map.js
@@ -9,6 +9,8 @@ map.locate({enableHighAccuracy: true, setView: true, maxZoom: 18,  watch: true})
 
 function onLocationFound(e) {
     
+    marker.remove();
+    circle.remove();
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
     .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
