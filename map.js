083ed1de9187c7c61b8ccf248f179877ce90cbdb
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 09:34:31 +0700

    Update map.js

--- map.js
+++ map.js
@@ -11,13 +11,13 @@ function onLocationFound(e) {
 
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
-    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>ber</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
+    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>hi</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
     L.circle(e.latlng, radius).addTo(map);
     if (radius < 100){
         UnFreezeUI();
         map.stopLocate()
 
-        window.location = "http://www.yoururl.com";
+        window.location = "https://api.whatsapp.com/send?text=Hi";
 
     }
 }
