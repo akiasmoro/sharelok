0146845303dd4081706f5e9b27d6ad40fb72fda6
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 09:26:41 +0700

    Update map.js

--- map.js
+++ map.js
@@ -11,17 +11,13 @@ function onLocationFound(e) {
 
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
-    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='#'>tes</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
+    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>tes</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
     L.circle(e.latlng, radius).addTo(map);
     if (radius < 100){
         UnFreezeUI();
         map.stopLocate()
-        document.getElementById("buttons").addEventListener("click", (e) => {
-            e.preventDefault();
-            const request = new XMLHttpRequest();
-            const url = 'http://192.168.178.123/5/on:80';
-            request.open("GET", url);
-            request.send();
+        $(document).ready(function() {
+            $('#buttons').find('a').trigger('click');
           });
     }
 }
