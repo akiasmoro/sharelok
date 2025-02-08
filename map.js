1ac6d93e4853080e092bc7ec71db809c9fb7b4e1
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 18:56:50 +0700

    Update map.js

--- map.js
+++ map.js
@@ -9,7 +9,6 @@ map.locate({enableHighAccuracy: true, setView: true, maxZoom: 18,  watch: true})
 
 function onLocationFound(e) {
 
-    if($('#mengerti').data('clicked')) {
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
     .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>hi</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
@@ -17,11 +16,9 @@ function onLocationFound(e) {
     if (radius < 100){
         UnFreezeUI();
         map.stopLocate()
-
-        window.location = "whatsapp://send?text=https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok adalah : "  + radius + " meter. Powered by BPN Lampung Timur";
+        window.open("whatsapp://send?text=https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur", "_blank");
 
     }
-}
 }
 
 
