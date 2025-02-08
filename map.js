5a831ec621c53adf2f47c32d2f04de42a8edee11
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 19:31:11 +0700

    Update map.js

--- map.js
+++ map.js
@@ -8,16 +8,19 @@ L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {
 map.locate({enableHighAccuracy: true, setView: true, maxZoom: 18,  watch: true});
 
 function onLocationFound(e) {
-
+    FreezeUI({ text: 'Mencari Koordinat' }); // Freeze with a custom text
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
-    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>hi</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
+    .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter, Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
     L.circle(e.latlng, radius).addTo(map);
-    if (radius < 100){
+    if (radius < 10){
         UnFreezeUI();
         map.stopLocate()
-        window.open("whatsapp://send?text=https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur", "_blank");
-
+        Swal.fire({
+            title: "Berhasil mendapatkan koordinat.",
+            icon: "success",
+          });
+          window.location.href = "whatsapp://send?text=https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)";
     }
 }
 
