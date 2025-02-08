ced1c54d2fe9f2f7c87ce261d5a01eb900d44b54
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2025-02-08 09:42:10 +0700

    Update map.js

--- map.js
+++ map.js
@@ -9,9 +9,7 @@ map.locate({enableHighAccuracy: true, setView: true, maxZoom: 18,  watch: true})
 
 function onLocationFound(e) {
 
-    $('#mengerti').on('click',function(){
-
-     
+    if($('#mengerti').data('clicked')) {
     var radius = e.accuracy.toFixed(2);
     L.marker(e.latlng).addTo(map)
     .bindPopup("Akurasi anda adalah : <b>"  + radius + "</b> meter,<a id='buttons' href='https://google.com'>hi</a> Klik <button class='btn btn-success btn-sm' onclick='Copy()'>Copy</button><div style='display:none' id='copydata'>https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok  adalah : "  + radius + " meter. Powered by BPN Lampung Timur)</div>").openPopup();
@@ -23,7 +21,7 @@ function onLocationFound(e) {
         window.location = "whatsapp://send?text=https://www.google.com/maps/place/"+ e.latlng.lat.toFixed(5) +","+ e.latlng.lng.toFixed(5) +" (Akurasi Sharelok adalah : "  + radius + " meter. Powered by BPN Lampung Timur";
 
     }
-})
+}
 }
 
 
